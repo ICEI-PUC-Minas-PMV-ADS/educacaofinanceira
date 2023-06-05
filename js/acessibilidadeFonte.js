6eaede805f7b89aa357d29865ba4dbc67c8fea6d
TalitaDevsPuc <128756637+TalitaDevsPuc@users.noreply.github.com>
2023-06-05 15:27:04 -0300

    Update acessibilidadeFonte.js

--- js/acessibilidadeFonte.js
+++ js/acessibilidadeFonte.js
@@ -3,7 +3,7 @@ function aumentarFonte() {
     var elemento = $('p, h3, h4');
     var fonteAtual = parseInt(elemento.css('font-size'));
     var novoTamanho = fonteAtual + 2;
-    if (novoTamanho <= 18) {
+    if (novoTamanho <= 16) {
         elemento.css('font-size', novoTamanho + 'px');
     }
 }
@@ -12,7 +12,7 @@ function diminuirFonte() {
     var elemento = $('p, h3, h4');
     var fonteAtual = parseInt(elemento.css('font-size'));
     var novoTamanho = fonteAtual - 2;
-    if (novoTamanho >= 10) {
+    if (novoTamanho >= 12) {
         elemento.css('font-size', novoTamanho + 'px');
     }
 }
